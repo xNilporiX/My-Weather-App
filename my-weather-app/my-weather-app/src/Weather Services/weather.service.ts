67185774e85666d186eb46c752a1c7608c3845be
xNilporiX <aninda400@gmail.com>
2023-03-06 14:39:11 +1030

    added ios android and segments

--- my-weather-app/my-weather-app/src/Weather Services/weather.service.ts
+++ my-weather-app/my-weather-app/src/Weather Services/weather.service.ts
@@ -6,11 +6,10 @@ import { IWeatherMain } from '../app/Types';
   providedIn: 'root'
 })
 export class WeatherService {
-  latitude = '-37.80';
-  longitude = '144.96';
+
   constructor(private _HttpCLient: HttpClient) { }
-  testWeatherApi(){
-    return this._HttpCLient.get<IWeatherMain>(`${API_ENDPOINT}latitude=${this.latitude}&longitude=${this.longitude}&hourly=temperature_2m&hourly=temperature_2m,rain,showers,snowfall,snow_depth,visibility&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,showers_sum,snowfall_sum&current_weather=true&precipitation_unit=inch&timezone=auto`);
+  getWeather(latitude: string, longitude: string){
+    return this._HttpCLient.get<IWeatherMain>(`${API_ENDPOINT}latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&hourly=temperature_2m,rain,showers,snowfall,snow_depth,visibility&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,showers_sum,snowfall_sum&current_weather=true&precipitation_unit=inch&timezone=auto`);
   }
 }
 const API_ENDPOINT = 'https://api.open-meteo.com/v1/forecast?';
--- my-weather-app/my-weather-app/src/app/home/home.page.ts
+++ my-weather-app/my-weather-app/src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { WeatherService } from '../../Weather Services/weather.service';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -8,17 +9,32 @@ import { WeatherService } from '../../Weather Services/weather.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  latitude: string = '0';
+  longitude: string = '0';
+  selectedSegment : string ='today';
 
-  constructor(private _http:HttpClient, private _weatherService:WeatherService) {
-    this.getWeather();
+  constructor(private _http: HttpClient, private _weatherService: WeatherService) {
+    this.currentLocation();
+    
   }
 
-  getWeather() {
-    this._weatherService.testWeatherApi().subscribe(
-      (data)=>{
-        console.log(data);
-      }
-    )
+  async currentLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.latitude = coordinates.coords.latitude.toPrecision();
+    this.longitude = coordinates.coords.longitude.toPrecision();
+    this.getWeather(this.latitude, this.longitude);
+  }
+  getWeather(latitude: string, longitude: string) {
+    {
+      this._weatherService.getWeather(latitude, longitude).subscribe(
+        (data) => {
+          console.log(data);
+        }
+      )
+    }
   }
 
+  segmentChanged(event:any){
+    this.selectedSegment = event.detail.value;
+  }
 }
